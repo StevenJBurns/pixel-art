4c3ebf66c4126b6e8c70ada89084f74effe561c4
Steven J Burns <stevenjburns@msn.com>
2018-04-18 01:49:22 -0600

    Moved original layout for color selection into ColorSelect component template

--- src/components/ColorSelect.vue
+++ src/components/ColorSelect.vue
@@ -1,6 +1,14 @@
 <template>
-
-
+  <div id="divColorSelect">
+    <h2>Color Palettes</h2>
+    <hr />
+    <h4>W3C Named Colors</h4>
+    <div id="divW3C"></div>
+    <hr />
+    <span>Or choose from 16,777,216 colors here...</span>
+    <input type="color" name="inputColorSelect">
+    <hr />
+  </div>
 </template>
 
 <script>
