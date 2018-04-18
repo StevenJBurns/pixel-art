09aaf01f5149847f4b7e4d0b7b09e1120d74509f
Steven J Burns <stevenjburns@msn.com>
2018-04-18 05:03:11 -0600

    Page CSS now working

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,7 @@
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
-    color: #2c3e50;
+    color: #DFDFDF;
     margin-top: 60px;
   }
 </style>
