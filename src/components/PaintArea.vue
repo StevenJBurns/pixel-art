ce090945828c5e3723dd1ea0fb0ff28fd4cd4920
Steven J Burns <stevenjburns@msn.com>
2018-04-18 05:09:28 -0600

    Removed index.html comment; copied original paint area html into component

--- src/components/PaintArea.vue
+++ src/components/PaintArea.vue
@@ -1,6 +1,12 @@
 <template>
-
-
+  <div id="divPaintArea">
+    <div id="divGrid"></div>
+    <hr />
+    <h4>Current Color</h4>
+    <div id="divCurrentColor">
+      <span id="spanCurrentColor">Gray</span>
+    </div>
+  </div>
 </template>
 
 <script>
