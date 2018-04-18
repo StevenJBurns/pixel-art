b13d3e7919944ab6ce2bb90795ba042713622b74
Steven J Burns <stevenjburns@msn.com>
2018-04-18 01:44:06 -0600

    Changed App to main vs div; created skeleton for ColorSelect component; fixed App data()

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div id="app">
+  <main id="app">
     <img src="./assets/logo.png">
     <router-view/>
-  </div>
+  </main>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    name: 'App',
+    data() {
+      return {
+        "currentColor" : '',
+        "recentColors" : []
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
