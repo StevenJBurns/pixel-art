094b851b031ab18d6bccfae12894a2d29e411306
StevenJBurns <stevenjburns@msn.com>
2018-08-24 13:27:40 -0600

    More package updates via NPM; minor clean up in comments and styles after a long break from this project and I almost know what Im doing in SPA development now...

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
+/* Vue Imports */
 import Vue from "vue";
+
+/* Component Imports */
 import App from "./App";
+
 
 Vue.config.productionTip = false;
 
-// State management without Vuex
+/* State management without Vuex -- the eventBus */
+/* Creates a new Vue instance to carry state */
 export const eventBus = new Vue({});
 
 /* eslint-disable no-new */
+
 new Vue({
   el: '#app',
   render: h => h(App)
