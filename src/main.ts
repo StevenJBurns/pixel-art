fa0f0d709dbbce726d3bf017989f1e8ad9939df5
StevenJBurns <stevenjburns@msn.com>
2024-02-17 08:37:16 -0600

    chore: install and configure Vuetify

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import "./style.css";
 
-createApp(App).mount("#vue-root");
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+
+createApp(App)
+  .use(createVuetify())
+  .mount("#vue-root");
